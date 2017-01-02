576b4791e33d7603c1588c4a723245598e62421e
martinv95 <kombt@citromail.hu>
2017-01-02 02:02:32 +0100

    m

---
+++ us4/Assets/RandomRock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRock : MonoBehaviour {
+
+    public GameObject gobj;
+    public Vector3 Center;
+    public Vector3 GeneratedRange;
+    public Vector3 RockSize;
+    public int MinRockNumber;
+    public int MaxRockNumber;
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < Random.Range(MinRockNumber, MaxRockNumber); i++)
+        {
+            Spawn();
+        }        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Spawn() {
+        Vector3 pos = Center + new Vector3(Random.Range(0, GeneratedRange.x), 0, Random.Range(0, GeneratedRange.z));
+        gobj.transform.localScale = new Vector3(RockSize.x, RockSize.y, RockSize.x);
+        Instantiate(gobj, pos, Quaternion.identity);
+    }
+}
